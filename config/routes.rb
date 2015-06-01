efe40bc9bc5443600a0a2b4a5e482b9d39c16015
Noah Finberg <noahfinberg@gmail.com>
2015-06-01 15:19:30 +0000

    moved new section to modal

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Flixter::Application.routes.draw do
       resources :lessons, :only => [:new, :create]
     end
     resources :courses, :only => [:new, :create, :show] do
-      resources :sections, :only => [:new, :create]
+      resources :sections, :only => [:create]
     end
   end
 
